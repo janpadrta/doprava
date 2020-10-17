e9e536264e9a4aade36ab369b05f8212ede7800d
janpadrta <jan.padrta.82@gmail.com>
2020-10-18 00:05:40 +0200

    hotfix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("transition");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
